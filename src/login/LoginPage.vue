99802cb0c95e909db336836e4ea6013eae078d58
unknown <cheval_irc@hotmail.com>
2018-09-28 22:56:45 +0200

    Test comming

--- src/login/LoginPage.vue
+++ src/login/LoginPage.vue
@@ -22,32 +22,31 @@
 </template>
 
 <script>
-import { mapState, mapActions } from 'vuex'
-
-export default {
-    data () {
-        return {
-            username: '',
-            password: '',
-            submitted: false
-        }
-    },
-    computed: {
-        ...mapState('account', ['status'])
-    },
-    created () {
-        // reset login status
-        this.logout();
-    },
-    methods: {
-        ...mapActions('account', ['login', 'logout']),
-        handleSubmit (e) {
-            this.submitted = true;
-            const { username, password } = this;
-            if (username && password) {
-                this.login({ username, password })
+    import { mapState, mapActions } from 'vuex'
+    export default {
+        data () {
+            return {
+                username: '',
+                password: '',
+                submitted: false
+            }
+        },
+        computed: {
+            ...mapState('account', ['status'])
+        },
+        created () {
+            // reset login status
+            this.logout();
+        },
+        methods: {
+            ...mapActions('account', ['login', 'logout']),
+            handleSubmit (e) {
+                this.submitted = true;
+                const { username, password } = this;
+                if (username && password) {
+                    this.login({ username, password })
+                }
             }
         }
-    }
-};
+    };
 </script>
