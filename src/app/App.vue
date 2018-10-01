177e80a2b71929929f858a62722225415b348c74
unknown <cheval_irc@hotmail.com>
2018-10-01 16:40:43 -0500

    Componente Header,Menu,Footer

--- src/app/App.vue
+++ src/app/App.vue
@@ -1,5 +1,7 @@
 <template>
     <div class="jumbotron">
+        <AppHeader></AppHeader>
+        <AppMenu></AppMenu>
         <div class="container">
             <div class="row">
                 <div class="col-sm-6 offset-sm-3">
@@ -8,14 +10,24 @@
                 </div>
             </div>
         </div>
+        <AppFooter></AppFooter>
     </div>
 </template>
 
 <script>
+//import Vue from "vue";
 import { mapState, mapActions } from 'vuex'
+import AppHeader from "../components/core/header/index";
+import AppMenu from "../components/core/menu/index";
+import AppFooter from "../components/core/footer/index";
 
 export default {
     name: 'app',
+    components: {
+        AppHeader,
+        AppMenu,
+        AppFooter
+    },
     computed: {
         ...mapState({
             alert: state => state.alert
---
+++ src/components/core/header/index.vue
@@ -0,0 +1,14 @@
+<template >
+    <div class="text-center">
+        <p>Header</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'AppHeader',
+    };
+</script>
+
+<style>
+</style>
